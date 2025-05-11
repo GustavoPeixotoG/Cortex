007bccc18541db4c87666f9cb9e1a7ebbd0596d5
GustavoPeixotoG <gustavopeixoto0000@gmail.com>
2025-05-11 13:05:13 -0300

    updated flake, tlp, intel

--- hm/minecraft-overlay.nix
+++ hm/minecraft-overlay.nix
@@ -1,7 +1,7 @@
 {
   nixpkgs.overlays = [
     (final: prev: {
-      unstable.prismlauncher-unwrapped = prev.prismlauncher-unwrapped.overrideAttrs (oa: let
+      prismlauncher-unwrapped = prev.prismlauncher-unwrapped.overrideAttrs (oa: let
         crackedPatch = final.fetchurl {
           url = "https://github.com/Misterio77/PrismLauncher/commit/2051b0b886b70d4efa9fbebc22d7d2fbe1e89255.diff";
           hash = "sha256-9/yYOPUkcHD5kL2PN9Ri8TsuthwbhHj4erx0wrr2mPQ=";
